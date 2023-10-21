0dca36fb7d8c9b21d17c5d84752494f4d2377e17
Franco Fichtner <franco@opnsense.org>
2023-10-21 08:03:25 +0200

    lang: wrong language selected in poeditor

--- Scripts/fetch_po_files.py
+++ Scripts/fetch_po_files.py
@@ -53,14 +53,14 @@ if 'response' in r and 'list' in r:
         if tmp == 'pt_BR.po':
             targets['pt-br'] = filename
         elif tmp == 'zh_TW.po':
-            targets['zh-Hant'] = filename
+            targets['zh-TW'] = filename
         else:
             targets[tmp.split('_')[0]] = filename
     for lang in r['list']:
         code = lang['code'].split('-')[0]
         if lang['code'] == 'pt-br':
             code = lang['code']
-        elif lang['code'] == 'zh-Hant':
+        elif lang['code'] == 'zh-TW':
             code = lang['code']
         if code not in targets:
             print("Skipped %(code)s (percentage complete : %(percentage).2f)" % lang)
